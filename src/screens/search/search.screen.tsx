5469986d508077a8c49ad896eb1670d5857c7d90
Kien Duy <buikienduy2020@gmail.com>
2025-03-14 23:17:28 +0700

    [EXPLORE] Xây giao diện (Có thể bỏ đi)

--- src/screens/search/search.screen.tsx
+++ src/screens/search/search.screen.tsx
@@ -1,14 +1,73 @@
-import { Text, View } from "react-native";
+import { FlatList, Image, Text, View } from "react-native";
 import SearchStyle from "./styles/search.style";
+import { useEffect, useState } from "react";
+import { CategoryType, ProductType } from "@/src/data/types/global";
+import axios from "axios";
+import { Stack } from "expo-router";
+import { useHeaderHeight } from "@react-navigation/elements"
+type Props = {
 
-const SearchScreen = () => {
+}
+
+const SearchScreen = (props: Props) => {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [categories, setCategories] = useState<CategoryType[]>([]);
+    const [products, setProducts] = useState<ProductType[]>([]);
+
+    useEffect(() => {
+        fetchCategories();
+        fetchProducts();
+        setIsLoading(false)
+    }, [])
+
+    const fetchProducts = async () => {
+        try {
+            const url = `http://192.168.0.101:8000/products`;
+            const response = await axios.get(url);
+            setProducts(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const fetchCategories = async () => {
+        try {
+            const url = `http://192.168.0.101:8000/categories`;
+            const response = await axios.get(url);
+            setCategories(response.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const headerHeight = useHeaderHeight();
     return (
-        <View style={style.container}>
-            <Text>Màn hình tìm kiếm</Text>
-        </View>
+        <>
+            <Stack.Screen
+                options={{
+                    headerShown: true,
+                    headerTransparent: true,
+                    headerTitleAlign: 'center'
+                }}
+            />
+            <View style={[styles.container, { marginTop: headerHeight }]}>
+                <FlatList
+                    data={categories}
+                    keyExtractor={(item) => item.id.toString()}
+                    showsVerticalScrollIndicator={false}
+                    style={{ marginBottom: 60 }}
+                    renderItem={({ index, item }) => (
+                        <View style={styles.itemWrapper}>
+                            <Text style={styles.itemTitle}>{item.name}</Text>
+                            <Image source={{ uri: item.image }} style={styles.itemImage} />
+                        </View>
+                    )}
+                />
+            </View>
+        </>
     );
 };
 
-const style = SearchStyle;
+const styles = SearchStyle;
 
 export default SearchScreen;
--- src/screens/search/styles/search.style.ts
+++ src/screens/search/styles/search.style.ts
@@ -1,10 +1,32 @@
+import { CommonColors } from "@/src/common/resource/colors";
+import { Fonts } from "@/src/common/resource/fonts";
 import { StyleSheet } from "react-native";
 
 const SearchStyle = StyleSheet.create({
     container: {
         flex: 1,
+        paddingHorizontal: 20
+    },
+    itemWrapper: {
+        flexDirection: 'row',
+        justifyContent: "space-between",
         alignItems: 'center',
-        justifyContent: 'center'
+        backgroundColor: CommonColors.extraLightGray,
+        padding: 10,
+        borderRadius: 10,
+        marginBottom: 20
+    },
+    itemTitle: {
+        fontSize: 16,
+        fontWeight: '500',
+        fontFamily: Fonts.POPPINS_MEDIUM,
+        lineHeight: 24,
+        color: CommonColors.black
+    },
+    itemImage: {
+        width: 100,
+        height: 100,
+        borderRadius: 10
     }
 });
 
