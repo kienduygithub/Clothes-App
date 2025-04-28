ba757104ae9d0b39d7a96c2a7872c787217e6c13
Kien Duy <buikienduy2020@gmail.com>
2025-04-28 16:29:26 +0700

    Thêm xử lý refresh token

--- src/common/config/axios.config.ts
+++ src/common/config/axios.config.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, AxiosRequestConfig } from "axios";
 import { AppConfig } from "./app.config";
 import { HandleHttp } from "../utils/handle-http";
+import { HttpCode } from "../resource/http-code";
 
 const appConfig = new AppConfig();
 
@@ -26,7 +27,42 @@ CustomAxios.interceptors.response.use(
     (response) => {
         return response.data;
     },
-    (error: AxiosError) => {
+    async (error: AxiosError) => {
+        const originalRequest = error.config as AxiosRequestConfig & { _retry?: boolean };
+        if (error.response?.status === HttpCode.UNAUTHORIZED && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                const refreshToken = await new AppConfig().getRefreshToken();
+                if (!refreshToken) {
+                    throw new Error('No refresh token available');
+                }
+                const domain = new AppConfig().getDomain();
+                const response = await axios.post(`${domain}/auth/refresh/mobile`, {
+                    ['refresh-token']: refreshToken
+                });
+                console.log('Chạy Refresh token...');
+                const { access_token: newAccessToken } = response.data;
+                if (newAccessToken) {
+                    console.log(newAccessToken);
+                    await new AppConfig().setAccessToken(newAccessToken);
+                }
+
+                if (originalRequest) {
+                    originalRequest.headers = originalRequest.headers ?? {};
+                    originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+                    return CustomAxios(originalRequest);
+                }
+                throw new Error('Original request is undefined');
+            } catch (error) {
+                await new AppConfig().clear();
+                console.log('>>> Refresh token failed', error);
+
+                return Promise.reject({
+                    message: 'Session expired, please log in again',
+                    status: HttpCode.UNAUTHORIZED
+                })
+            }
+        }
         return Promise.reject(error.response?.data);
     }
 )
