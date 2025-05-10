730a8fddcefa7af24962a47bca52ac5413a51c4d
Kien Duy <buikienduy2020@gmail.com>
2025-05-11 00:58:15 +0700

    [Notification] Fix lỗi chạm list mà k load thông báo

--- src/screens/notification/notification.screen.tsx
+++ src/screens/notification/notification.screen.tsx
@@ -3,7 +3,7 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import { FlatList, RefreshControl, Text, View } from "react-native";
 import NotificationStyle from "./notification.style";
 import { useHeaderHeight } from "@react-navigation/elements"
-import { Ionicons } from "@expo/vector-icons";
+import { AntDesign, Feather, Ionicons } from "@expo/vector-icons";
 import { CommonColors } from "@/src/common/resource/colors";
 import Animated, { FadeInDown } from "react-native-reanimated";
 import { NotificationModel } from "@/src/data/model/notification.model";
@@ -18,6 +18,7 @@ import * as UserActions from "@/src/data/store/actions/user/user.action";
 import { UserStoreState } from "@/src/data/store/reducers/user/user.reducer";
 import { NotificationType } from "@/src/common/resource/notification";
 import { LinearGradient } from "expo-linear-gradient";
+import { formatPriceRender } from "@/src/common/utils/currency.helper";
 
 const NotificationScreen = () => {
     const dispatch = useDispatch();
@@ -41,7 +42,7 @@ const NotificationScreen = () => {
         } else {
             setNotifications(notificationSelector.notifications);
         }
-    }, [notificationSelector.isLoaded]);
+    }, []);
 
 
     const fetchNotifications = async (page: number) => {
@@ -52,7 +53,7 @@ const NotificationScreen = () => {
 
             isFetching.current = true;
             const response = await NotificationMana.fetchNotificationByUser(page, pagination.limit);
-            setNotifications(response.get('notifications'));
+            setNotifications(prev => [...prev, ...response.get('notifications')]);
             dispatch(NotificationActions.SaveNotifications(response.get('notifications')));
 
             setPagination(response.get('pagination'));
@@ -69,12 +70,13 @@ const NotificationScreen = () => {
 
     const handleRefreshNotifications = useCallback(() => {
         setRefresh(true);
+        setNotifications([]);
         isEndReachedList.current = false;
         fetchNotifications(1);
     }, [])
 
     const handleLoadMore = () => {
-        if (!isEndReachedList.current && !isFetching.current && pagination.currentPage < pagination.totalPages) {
+        if (!isEndReachedList.current && !isFetching.current && pagination.currentPage <= pagination.totalPages) {
             fetchNotifications(pagination.currentPage + 1);
         }
     };
@@ -83,23 +85,27 @@ const NotificationScreen = () => {
         const isUnread = !item.is_read;
         const timeAgo = formatDate(item.created_at ?? new Date());
 
-        let iconName: "cart-outline" | "hourglass-outline" = "cart-outline";
+        let icon = (
+            <Feather name="package" size={24} color={CommonColors.primary} />
+        )
         let title = "";
         let message = "";
 
         if (item.type === NotificationType.ORDER_NEW) {
             title = "Đơn hàng mới";
-            message = `Bạn đã mua ${item.data?.order_id ? `#${item.data.order_id}` : ''} thành công. Tổng: ${item.data?.order_total?.toLocaleString() || 'N/A'} VNĐ`;
+            message = `Bạn đã mua ${item.data?.order_id ? `#${item.data.order_id}` : ''} thành công. Tổng: ${formatPriceRender(item.data?.order_total ?? 0)} VNĐ`;
         } else if (item.type === NotificationType.ORDER_CANCELED) {
-            iconName = "hourglass-outline";
+            icon = (
+                <Feather name="x-octagon" size={24} color={CommonColors.red} />
+            )
             title = "Hủy đơn hàng";
-            message = `Đơn hàng ${item.data?.order_id ? `#${item.data.order_id}` : ''} đã bị hủy ${item.data?.reason ? `(${item.data.reason})` : ''}. Tổng: ${item.data?.order_total?.toLocaleString() || 'N/A'} VNĐ`;
+            message = `Đơn hàng ${item.data?.order_id ? `#${item.data.order_id}` : ''} đã bị hủy ${item.data?.reason ? `(${item.data.reason})` : ''}.`;
         }
 
         return (
             <Animated.View entering={FadeInDown.delay(index * 100).duration(300)} style={styles.notificationItem}>
                 <View style={styles.iconContainer}>
-                    <Ionicons name={iconName} size={24} color={CommonColors.primary} />
+                    {icon}
                 </View>
                 <View style={styles.contentContainer}>
                     <Text style={styles.title}>{title}</Text>
@@ -129,7 +135,7 @@ const NotificationScreen = () => {
                         <RefreshControl refreshing={refresh} onRefresh={handleRefreshNotifications} />
                     }
                     onEndReached={handleLoadMore}
-                    onEndReachedThreshold={0.5}
+                    onEndReachedThreshold={0.1}
                     ListEmptyComponent={
                         <Text style={styles.emptyText}>Không có thông báo nào</Text>
                     }
